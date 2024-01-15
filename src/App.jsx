8d96cc3d97de951d05eafa8bed3c57fd3bb28af3
MohammedAlZahra <mohammed.alzahra@dci.education>
2024-01-15 11:13:17 +0100

    add functionality (added addItem & updatelItem)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header.jsx";
 import DonesContainer from "./components/DonesContainer.jsx";
 import TodosContainer from "./components/TodosContainer.jsx";
-
+// ==========================
 function App() {
-  const tasks = [
+  const tasksArr = [
     { id: 0, text: "Wash my face!", done: false },
     { id: 1, text: "Walk the dog", done: false },
     { id: 2, text: "Pay the rent", done: false },
@@ -14,6 +15,29 @@ function App() {
     { id: 6, text: "Finish reading my book", done: true },
     { id: 7, text: "Make more moneys", done: true },
   ];
+  // ===========================================
+  const [tasks, setTasks] = useState(tasksArr);
+  // ===============================
+  const addItem = (value) => {
+    const newItem = {
+      id: tasks.length,
+      text: value,
+      done: false,
+    };
+    return setTasks(newItem, ...tasks);
+  };
+
+  const updateItem = (id) => {
+    const updatedItem = tasks.map((item) => {
+      if (item.di === id) {
+        item.done = !item.done;
+      }
+    });
+    setTasks(updatedItem);
+  };
+
+  // ======================
+
   return (
     <div>
       <Header />
